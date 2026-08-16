d4e75b7b53872c2ab925a9877e3e92412ae3b736
jeetvaghela <jeetvaghela088@email.com>
2026-08-16 19:06:01 +0530

    Refactor customer page interactions and update E2E tests for product verification

--- tests/001.home-without-login.spec.js
+++ tests/001.home-without-login.spec.js
@@ -7,11 +7,11 @@ test('Verify Home Page', async ({ page, homepage }) => {
   await homepage.verifyHomePage();
   await homepage.clickOnProductsNav();
   await homepage.clickOnCartNav();
-  await homepage.searchProducts('Mechanical Keyboard RGB', 'Electronics');
+  await homepage.searchProducts('Mechanical Keyboard RGB');
   await homepage.clickOnProductImage();
   await homepage.verifyProductDetailPage();
   await homepage.clickAddToCart();
   await homepage.clickWishlist();
-
+  const productsGrid = this.page.getByTestId('products-grid');
 
 });
